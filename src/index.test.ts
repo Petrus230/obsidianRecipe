e7468d47ef6889fc7993f0e7cf7ed4f62d91c31c
Lluis <lluis.guivi@gmail.com>
2024-03-09 11:15:53 +0100

    Read ingredients

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,27 @@
-describe('Index', () => {
-  it('works', () => {
+describe('Recept note', () => {
+  it('has ingredientes', () => {
     //arrange
-    const input = false
+    const input = `# Ingredients:\n- Mantequilla\n- Huevos`
 
     //act
-    const result = input
+    const result = readIngredients(input)
 
     //assert
-    expect(result).toBeTruthy();
+    expect(result).toEqual(['Mantequilla', 'Huevos']);
   });
 });
+
+function readIngredients(noteContent: string) {
+  const ingredientLines = noteContent.split('\n').slice(1)
+
+  const ingredients: string[] = []
+  ingredientLines.forEach(ingredientLine => {
+
+    ingredients.push(readIngredientName(ingredientLine))
+  })
+  return ingredients
+}
+
+function readIngredientName(line: string){
+  return line.slice(1).trim()
+}
