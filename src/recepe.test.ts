6fb0e7b9c0e0d4068618d2b6c467429f50a0e243
Lluis <lluis.guivi@gmail.com>
2024-03-10 19:48:24 +0100

    Create Ingredient type

--- src/recepe.test.ts
+++ src/recepe.test.ts
@@ -4,39 +4,40 @@ import { calculateCalories, readIngredients } from "./recepe";
 describe('Recept', () => {
   it('has ingredientes', () => {
     //arrange
-    const input = `# Ingredients:\n- Harina\n- Huevos`
+    const input = `# Ingredients:\n- 200 Harina\n- 2 Huevos`
 
     //act
-    const ingredients = readIngredients(input)
+    const ingredients = readIngredients(input, foods)
 
     //assert
-    expect(ingredients).toEqual(['harina', 'huevos']);
+    expect(ingredients.length).toEqual(2);
   });
 
-  it('calculates total calories', () => {
+  it('calculates calories for the quantity which are using', () => {
     //arrange
-    const foods: { [key: string]: Food } = {
-      harina: {
-        name: "harina",
-        calories: 200,
-        fat: 0,
-        protein: 0,
-        carbohydrates: 0
-      },
-      huevos: {
-        name: "huevos",
-        calories: 50,
-        fat: 0,
-        protein: 0,
-        carbohydrates: 0
-      }
-    }
-    const input = `# Ingredients:\n- Harina\n- Huevos`
+    const repece = `# Ingredients:\n- 200 Harina`
 
     //act
-    const result = calculateCalories(input, foods)
+    const result = calculateCalories(repece, foods)
 
     //assert
-    expect(result).toEqual(250);
+    expect(result).toEqual(20);
   });
 });
+
+const foods: { [key: string]: Food } = {
+  harina: {
+    name: "harina",
+    calories: 0.1,
+    fat: 0,
+    protein: 0,
+    carbohydrates: 0
+  },
+  huevos: {
+    name: "huevos",
+    calories: 50,
+    fat: 0,
+    protein: 0,
+    carbohydrates: 0
+  }
+}
--- src/recepe.ts
+++ src/recepe.ts
@@ -1,27 +1,42 @@
 import { Food } from "./foods"
 
-export function calculateCalories(input: string, foods: { [name: string]: Food }) {
-  const ingredientes = readIngredients(input)
+type Ingredient = {
+  properties: Food,
+  quantity: number
+}
+
+export function calculateCalories(recepe: string, foods: { [name: string]: Food }) {
+  const ingredients = readIngredients(recepe, foods)
   let calories = 0
-  ingredientes.forEach(ingredientes=>{
-    calories += foods[ingredientes].calories
+  ingredients.forEach(ingrediente =>{
+    console.log(ingrediente)
+    calories += ingrediente.quantity * ingrediente.properties.calories
   })
 
   return calories
 }
 
 
-export function readIngredients(noteContent: string) {
+export function readIngredients(noteContent: string, foods: { [name: string]: Food }): Ingredient[] {
   const ingredientLines = noteContent.split('\n').slice(1)
 
-  const ingredients: string[] = []
+  const ingredients: Ingredient[] = []
   ingredientLines.forEach(ingredientLine => {
-
-    ingredients.push(readIngredientName(ingredientLine).toLocaleLowerCase())
+    const name = readIngredientName(ingredientLine)
+    const quantity = readIngredientQuantity(ingredientLine)
+    const ingredient: Ingredient = {
+      properties: foods[name],
+      quantity
+    }
+    ingredients.push(ingredient)
   })
   return ingredients
 }
 
 function readIngredientName(line: string) {
-  return line.slice(1).trim()
+  return line.slice(1).trim().split(" ")[1].toLocaleLowerCase()
+}
+
+function readIngredientQuantity(line: string): number {
+  return Number(line.slice(1).trim().split(" ")[0])
 }
